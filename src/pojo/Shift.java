f78903c59fd76a0ecf0479e6cf4137d4fe610e9f
nhoanglong20 <hoanglongmcs1@gmail.com>
2021-06-09 19:07:53 +0700

    add Shift hibernate config and its pojo.Shilf

---
+++ src/pojo/Shift.java
@@ -0,0 +1,56 @@
+package pojo;
+
+import java.sql.Time;
+import java.util.Objects;
+
+/**
+ * pojo
+ *
+ * @created by ASUS - StudentID : 18120449
+ * @Date 6/9/2021 - 6:55 PM
+ * @Description
+ */
+public class Shift {
+    private int shiftId;
+    private Time startAt;
+    private Time endAt;
+
+    public int getShiftId() {
+        return shiftId;
+    }
+
+    public void setShiftId(int shiftId) {
+        this.shiftId = shiftId;
+    }
+
+    public Time getStartAt() {
+        return startAt;
+    }
+
+    public void setStartAt(Time startAt) {
+        this.startAt = startAt;
+    }
+
+    public Time getEndAt() {
+        return endAt;
+    }
+
+    public void setEndAt(Time endAt) {
+        this.endAt = endAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Shift shift = (Shift) o;
+        return shiftId == shift.shiftId &&
+                Objects.equals(startAt, shift.startAt) &&
+                Objects.equals(endAt, shift.endAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(shiftId, startAt, endAt);
+    }
+}
